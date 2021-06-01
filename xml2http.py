6092e2f4eba1b09c2e852fde39c784eeb6958040
陈广生 <53112648+rootwhois@users.noreply.github.com>
2021-06-01 10:04:00 +0800

    Update xml2http.py

--- xml2http.py
+++ xml2http.py
@@ -3,20 +3,19 @@ import urllib
 import urllib.request
 import re
 import xml.etree.cElementTree as et
-url='https://rootwhois.cn/atom.xml'
-file_name='atom.xml'
+url='https://blog.rootwhois.cn/sitemap.xml'
+file_name='sitemap.xml'
 html=urllib.request.urlopen(url).read()
 html=html.decode('utf-8')
 with open(file_name, 'w') as f:
     f.write(html)
 
-tree_obj = et.parse("atom.xml")
+tree_obj = et.parse("sitemap.xml")
 root = tree_obj.getroot()
 # print(root.tag)
 file = open('baidu_urls.txt', 'w')
-for url in root.iter('{http://www.w3.org/2005/Atom}id'):
+for url in root.iter('{http://www.sitemaps.org/schemas/sitemap/0.9}loc'):
     # print(url.text)
-    url.text = url.text.replace('https://', 'https://www.')
     file.write(url.text + '\n')
 file.close()
 
